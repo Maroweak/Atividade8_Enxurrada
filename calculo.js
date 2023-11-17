397d258094c0f2046af3b24ab535bdf95e05bafb
João Pedro Coutinho <maroweakedinho@gmail.com>
2023-11-17 18:21:39 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,13 @@
+const ano = 2023;
+
+const nome = prompt('Digite seu nome: ');
+const nascimento = prompt('Digite o ano que você nasceu: ');
+
+const idade = ano - nascimento;
+
+let info = `Olá, ${nome}, sua idade é de ${idade} anos.`
+
+alert(`${info}`)
+
+
+
